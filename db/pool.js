062a04639b22e46688ed4a0e27a0541becab6d6e
dev-tope <charlestemitope@gmail.com>
2025-01-07 09:57:08 +0100

    env variables changed

--- db/pool.js
+++ db/pool.js
@@ -2,27 +2,26 @@ import pg from "pg";
 import dotenv from "dotenv";
 import fs from "fs";
 
-dotenv.config()
-
+dotenv.config();
 
 const { Pool } = pg;
 
-export const pool = new Pool ({
-  host: process.env.DATABASE_HOST,
-  database: process.env.DATABASE_NAME,
-  username: process.env.DATABASE_USER,
+export const pool = new Pool({
+  host: process.env.PROD_DATABASE_HOST,
+  database: process.env.PROD_DATABASE_NAME,
+  username: process.env.PROD_DATABASE_USER,
   password: process.env.PROD_DATABASE_PASSWORD,
-  ssl: process.env.DATABASE_SSL,
+  ssl: process.env.PROD_DATABASE_SSL,
 });
 
-console.log("Database User", process.env.DATABASE_USER);
+console.log("Database User", process.env.PROD_DATABASE_USER);
 console.log("Database Password", process.env.PROD_DATABASE_PASSWORD);
 
 (async () => {
   try {
-    const result = await pool.query('SELECT NOW()');
-    console.log('Connected to DB:', result.rows[0]);
-  } catch (err){
-    console.error('Database connection error', err)
-  } 
-})()
+    const result = await pool.query("SELECT NOW()");
+    console.log("Connected to DB:", result.rows[0]);
+  } catch (err) {
+    console.error("Database connection error", err);
+  }
+})();
