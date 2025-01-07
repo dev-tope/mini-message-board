56df2c97bafa2fed558a3b57d7a98f5c80dc085f
dev-tope <charlestemitope@gmail.com>
2025-01-07 08:36:36 +0100

    changes made to db pool

--- db/pool.js
+++ db/pool.js
@@ -1,5 +1,6 @@
 import pg from "pg";
 import dotenv from "dotenv";
+import fs from "fs";
 
 dotenv.config()
 
@@ -7,10 +8,22 @@ dotenv.config()
 const { Pool } = pg;
 
 export const pool = new Pool ({
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD, //hardcoded because hosting refuses to connect with env variable
-  port: process.env.DB_PORT,
-  database: process.env.DATABASE_NAME,
-  ssl: process.env.DATABASE_SSL
-}) 
+  host: process.env.PROD_DATABASE_HOST,
+  user: process.env.PROD_DATABASE_USER,
+  password: process.env.PROD_DATABASE_PASSWORD, //hardcoded because hosting refuses to connect with env variable
+  // port: process.env.DB_PORT,
+  database: process.env.PROD_DATABASE_NAME,
+  ssl: process.env.PROD_DATABASE_SSL
+});
+
+console.log("Database Password", process.env.DATABASE_USER);
+console.log("Database Password", process.env.DATABASE_PASSWORD);
+
+(async () => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    console.log('Connected to DB:', result.rows[0]);
+  } catch (err){
+    console.error('Database connection error', err)
+  } 
+})()
--- db/queries.js
+++ db/queries.js
@@ -2,13 +2,13 @@ import { pool } from "./pool.js";
 
 async function getAllMessages() {
   // console.log("Pool is :", pool)
-  const { rows } = await pool.query("SELECT * FROM message_details")
-  return rows
+  const { rows } = await pool.query("SELECT * FROM message_details");
+  return rows;
 }
 
 async function insertMessage(username, message) {
   const time = new Date();
-  await pool.query("INSERT INTO message_details (username, message, time) VALUES ($1, $2, $3)", [username, message, time])
+  await pool.query("INSERT INTO message_details (username, message, time) VALUES ($1, $2, $3)", [username, message, time]);
 }
 
 
